97774b98321494185291ba4f92e7348d66bc5e10
Lazar Dudic <lazar.dudic@yahoo.com>
2022-06-12 12:03:19 +0200

    Factory Method

---
+++ DesignPatterns/FactoryMethodPattern/index.php
@@ -0,0 +1,44 @@
+<?php
+
+abstract class Subscription
+{
+    abstract public function price();
+}
+
+class SubscriptionFactory
+{
+    protected array $availableSubsriptions = [
+        'free' => FreeSubscription::class,
+        'premium' => PremiumSubscription::class,
+    ];
+
+    public function create($subsription = 'free'): Subscription
+    {
+        if (! isset($this->availableSubsriptions[$subsription])) {
+            throw new \Exception('A subsription ' . $subsription . ' could not be found');
+        }
+
+        return new $this->availableSubsriptions[$subsription];
+    }
+}
+
+class FreeSubscription extends Subscription
+{
+    public function price()
+    {
+        return 0;
+    }
+}
+
+class PremiumSubscription extends Subscription
+{
+
+    public function price()
+    {
+        return 150;
+    }
+}
+
+
+$subsription = (new SubscriptionFactory())->create('premium');
+echo 'Price for ' . get_class($subsription) . ' is $' . $subsription->price();
