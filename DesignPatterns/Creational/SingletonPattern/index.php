59f5fd1d45d91f84e1d80fa73d19dddfe24fe623
Lazar Dudic <lazar.dudic@yahoo.com>
2022-06-13 11:31:59 +0200

    Singleton

---
+++ DesignPatterns/Creational/SingletonPattern/index.php
@@ -0,0 +1,33 @@
+<?php
+
+class Singleton
+{
+    private static $instances = [];
+
+    protected function __construct() { }
+
+    protected function __clone() { }
+
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+
+    public static function getInstance(): Singleton
+    {
+        $cls = static::class;
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+
+        return self::$instances[$cls];
+    }
+}
+
+$s1 = Singleton::getInstance();
+$s2 = Singleton::getInstance();
+if ($s1 === $s2) {
+    echo "Singleton works, both variables contain the same instance.";
+} else {
+    echo "Singleton failed, variables contain different instances.";
+}
